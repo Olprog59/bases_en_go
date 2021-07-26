526f446647c79cf296dea75138944153731a439d
Samuel Michaux <samuel.michaux@gmail.com>
2021-07-26 17:45:51 +0200

    defer

--- main.go
+++ main.go
@@ -3,27 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{
-		"Samuel",
-		"Nicolas",
-		"Patrick",
-		"Lony",
-		"Perséa",
+	fmt.Println("Start main()")
+	defer fmt.Println("End main")
+
+	a := true
+
+	if a {
+		fmt.Println("je suis dans une condition!!")
+		return
 	}
 
-	//for i := 0; i < len(names); i++ {
-	//	fmt.Printf("Name : %s\n", names[i])
-	//}
-
-	//for index, value := range names {
-	//	fmt.Printf("Name : %s à l'index %d\n", value, index)
-	//}
-
-	for _, value := range names {
-		fmt.Printf("Name : %s\n", value)
-	}
-	fmt.Println("----------------------")
-	for index := range names {
-		fmt.Printf("Name : %s\n", names[index])
-	}
 }
