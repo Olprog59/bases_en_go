095ab66ee2db453031433216f428b84157531f97
Samuel Michaux <samuel.michaux@gmail.com>
2021-07-26 13:42:57 +0200

    declaration et affectation variable

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"bases_en_go/output"
-	"strings"
+	"fmt"
 )
 
 func main() {
-	// fmt.Println("Bienvenue")
-	output.PrintLn(strings.ToUpper("Premier pas en go"))
+	var age int // déclaration
+	age = 40    // affectation
+
+	// var nom string = "Michaux"
+
+	// var prenom = "Samuel"
+
+	// var prenom, nom string = "samuel", "Michaux"
+
+	var prenom, nom = "samuel", "Michaux"
+
+	codePostal := "59560"
+
+	isOk := false
+
+	fmt.Println(prenom, nom, codePostal, isOk, age)
+
+	var a int8 = -128 // 8 bits => -128 à 127
+	var b uint8 = 255 // 8 bits => 0 à 255
+
+	fmt.Println(a, b)
 }
