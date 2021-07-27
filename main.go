2dcb2d2be654701f99b0962a89b8acd8391fba26
Samuel Michaux <samuel.michaux@gmail.com>
2021-07-27 12:06:24 +0200

    struct - 1

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import "fmt"
 
+type User struct {
+	firstname string
+	lastname  string
+	age       int
+}
+
 func main() {
-	fmt.Println("Start main()")
-	defer fmt.Println("End main")
+	var sam User
+	sam.firstname = "samuel"
+	//sam.lastname = "michaux"
+	sam.age = 40
 
-	a := true
+	fmt.Printf("%#+v\n", sam)
 
-	if a {
-		fmt.Println("je suis dans une condition!!")
-		return
+	lony := User{
+		"lony",
+		"michaux",
+		16,
 	}
+	fmt.Printf("%#+v\n", lony)
 
+	sabrina := User{
+		firstname: "Sabrina",
+	}
+	fmt.Printf("%#+v\n", sabrina)
 }
